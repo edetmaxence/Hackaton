620c058280222663133570a20a6f4c8eb4720a3f
GaelleBriet <gaelle.briet@protonmail.com>
2022-09-24 12:47:59 +0200

    homepage

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\RestaurantRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,12 @@ use Symfony\Component\Routing\Annotation\Route;
 class MainController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(RestaurantRepository $restaurantRepository): Response
     {
-        return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
-        ]);
+        $restaurants = $restaurantRepository->findBy([], ['note' => 'DESC']);
+        return $this->render(
+            'main/index.html.twig',
+            ['restaurants' => $restaurants]
+        );
     }
 }
